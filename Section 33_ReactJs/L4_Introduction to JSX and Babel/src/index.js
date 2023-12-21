00f0ee5f5838e6802cc0ad534abac08daf31bb1d
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-12-21 09:37:08 -0600

    Add files via upload

---
+++ Section 33_ReactJs/L4_Introduction to JSX and Babel/src/index.js
@@ -0,0 +1,15 @@
+//NewWay Import ReactModule
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favorite foods</h1>
+    <ul>
+      <li>Fried Chicken</li>
+      <li>Wagyu A5 Steam</li>
+      <li>Fisha and Chips</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
