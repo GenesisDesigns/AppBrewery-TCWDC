8e4b98264853e643d078523b8e77a3c3b15b5bdc
GensisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-12-31 20:03:18 +0000

    Update index.js

--- Section 33_ReactJs/L10_ReactStylingPractice/src/index.js
+++ Section 33_ReactJs/L10_ReactStylingPractice/src/index.js
@@ -2,3 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../src/styles.css";
+
+let greetings;
+
+//js object
+const customStyles = {
+  color: "",
+  fontSize: "30px",
+  borderBottom: "5px solid black",
+};
+
+const pokemonName = "Pikachu";
+const date = new Date(2019, 1, 2, 7);
+const currentTime = date.getHours();
