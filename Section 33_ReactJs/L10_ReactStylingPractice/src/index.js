11e0c115f68e4c8c7eed96beffe9627d98506393
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-12-31 14:06:23 -0600

    Update index.js

--- Section 33_ReactJs/L10_ReactStylingPractice/src/index.js
+++ Section 33_ReactJs/L10_ReactStylingPractice/src/index.js
@@ -16,3 +16,16 @@ const pokemonName = "Pikachu";
 const date = new Date(2019, 1, 2, 7);
 const currentTime = date.getHours();
 
+console.log(date);
+
+if (currentTime >= 0 && currentTime < 12) {
+  greetings = `Good Morning ${pokemonName}, it's ${currentTime}:00 O'clock`;
+  customStyles.color = "red";
+} else if (currentTime >= 12 && currentTime < 18) {
+  greetings = `Good Afternoon ${pokemonName}, it's ${currentTime}:00 O'clock`;
+  customStyles.color = "green";
+} else {
+  greetings = `Good Evening ${pokemonName}, it's ${currentTime}:00 O'clock`;
+  customStyles.color = "blue";
+}
+
