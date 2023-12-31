5320d61f77549d68ca04b7033ef4a70d255ef7b4
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-12-31 14:06:07 -0600

    Update index.js

--- Section 33_ReactJs/L10_ReactStylingPractice/src/index.js
+++ Section 33_ReactJs/L10_ReactStylingPractice/src/index.js
@@ -2,3 +2,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../src/styles.css";
+
+let greetings;
+
+//js object
+const customStyles = {
+  color: "",
+  fontSize: "30px",
+  borderBottom: "5px solid black",
+};
