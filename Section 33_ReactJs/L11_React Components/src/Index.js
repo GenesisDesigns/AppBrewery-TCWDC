45510d9f7e565f3084a5ef79560e6549324007db
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2024-01-02 15:08:12 -0600

    Add files via upload

---
+++ Section 33_ReactJs/L11_React Components/src/Index.js
@@ -0,0 +1,7 @@
+//NewWay Import ReactModule
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import App from "../components/App"; //move 1 directory up 
+
+ReactDOM.render(<App />, document.getElementById("root"));
