cf1a07ab60d8b4ed44552f76c911c99fafa902f7
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2024-01-02 15:08:50 -0600

    Add files via upload

---
+++ Section 33_ReactJs/L11_React Components/components/App.jsx
@@ -0,0 +1,22 @@
+//NewWay Import ReactModule
+import React from "react";
+
+import "./styles.css";
+import Heading from "./Heading"; // dont need extension
+import List from "./List"; //dont need extension, use ./ b.c file in ssame folder on same level
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
