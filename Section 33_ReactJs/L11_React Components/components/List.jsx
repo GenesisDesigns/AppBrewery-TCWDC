8e721fa078beb5f1034e5c6c061583dd5fd57319
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2024-01-02 15:09:43 -0600

    Add files via upload

---
+++ Section 33_ReactJs/L11_React Components/components/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>Assassins Creed</li>
+        <li>God of War</li>
+        <li>Grand Theft Auto</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
